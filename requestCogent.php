c14e51bca6f5473c496b0cf2c7330b3c474530c7
Mel M <marti216@purdue.edu>
2014-04-05 16:12:25 -0400

    changed form action to doRequestAction

--- requestCogent.php
+++ requestCogent.php
@@ -12,7 +12,7 @@
 			<button>Submit</button>
 
 			<div id="requestCogentEntry1" class="cogentRequestForm">
-				<form action="requestCogent.php" method="post">
+				<form action="doRequestCogent.php" method="post">
 					<label for="requestType">Type</label><select name="requestType"><option>Type 1</option><option>Type 2</option><option>Type 3</option></select>
 					<label for="requestAmount">Amount</label><select name="requestAmount"><option>50</option><option>100</option><option>Other</option></select>
 					<label for="requestMemo">Memo</label><textarea name="requestMemo"></textarea>
